08ed659857c784a21e115ed12c1acebe50a780b2
Bogdan <pinchuk.brus@windowslive.com>
2020-09-01 11:06:00 +0300

    Added some unit tests and methods for get page size

--- Specialized_PDF_Editor.Tests/AnalysisPdfFile.cs
+++ Specialized_PDF_Editor.Tests/AnalysisPdfFile.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System.Diagnostics;
+using iTextSharp.text;
+using System.Runtime.InteropServices;
 
 namespace Specialized_PDF_Editor.Tests
 {
@@ -19,9 +21,9 @@ namespace Specialized_PDF_Editor.Tests
         [ClassInitialize]
         public static void InitializeClass(TestContext _)
         {
-            MainForm form = new MainForm(new string[] 
-            { 
-                new FileInfo(@"Resources\File_3_pages.pdf").FullName 
+            MainForm form = new MainForm(new string[]
+            {
+                new FileInfo(@"Resources\File_3_pages.pdf").FullName
             });
             form.LoadPdfForRead();
             streamC = form.streamC;
@@ -47,11 +49,79 @@ namespace Specialized_PDF_Editor.Tests
 
             // act
             analysis.ExtractData();
-            var actual = analysis.PageCount;
+            int actual = analysis.PageCount;
             Debug.WriteLine($"\nNumber of page: {actual}");
 
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        /// <summary>
+        /// Testing pdf file on size of page
+        /// </summary>
+        [TestMethod]
+        public void PageRotate_0degree()
+        {
+            // arrange
+            int expected = 0;
+
+            // act
+            analysis.ExtractData();
+            var actual = analysis.Pages;
+
+            // assert
+            foreach (var p in actual)
+                Assert.AreEqual(expected, p.Rotation);
+        }
+
+        /// <summary>
+        /// Testing pdf size in "mm"
+        /// </summary>
+        [TestMethod]
+        public void PageSize_297x210()
+        {
+            // arrange
+            double expected_w = 297,
+                expected_h = 210;
+
+            // act
+            analysis.ExtractData();
+            var actual = analysis.Pages;
+            double height, winth;
+
+            // assert
+            for (int i = 0; i < actual.Length; i++)
+            {
+                height = Utilities.PointsToMillimeters(actual[i].Size.GetHeight());
+                winth = Utilities.PointsToMillimeters(actual[0].Size.GetWidth());
+                Assert.AreEqual(expected_h, height, 1);
+                Assert.AreEqual(expected_w, winth, 1);
+
+                Debug.WriteLine($"\nPage {i + 1}:\n");
+                Debug.WriteLine($"\theight: {height:G4}");
+                Debug.WriteLine($"\twinth: {winth:G4}");
+            }
+        }
+
+        /// <summary>
+        /// Method for other review of document
+        /// </summary>
+        [TestMethod]
+        public void Testing()
+        {
+            analysis.ExtractData();
+
+            foreach (var s in analysis.Pages)
+                Debug.WriteLine("\n" + s.ToString() + "\n");
+        }
     }
 }
+
+//TODO: 1. Orientation of page
+//TODO: 2. Size of page
+//TODO: 3. Size of rectangle or board
+//TODO: 4. Metadata of file
+//TODO: 4.1. Date of create
+//TODO: 4.2. Creator
+//TODO: 4.3. Version of PDF
+//TODO: 4.4. Font in file ???
--- Specialized_PDF_Editor/Analysis.cs
+++ Specialized_PDF_Editor/Analysis.cs
@@ -1,9 +1,12 @@
-﻿using iText.Kernel.Pdf;
+﻿using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
 using iText.Layout;
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 [assembly: InternalsVisibleTo("Specialized_PDF_Editor.Tests")]
 namespace Specialized_PDF_Editor
@@ -17,7 +20,14 @@ namespace Specialized_PDF_Editor
         /// Stream for loaded file
         /// </summary>
         private MemoryStream stream = new MemoryStream();
+        /// <summary>
+        /// Information about every page
+        /// </summary>
+        internal PageInfo[] Pages { get; private set; }
 
+        /// <summary>
+        /// Page numbers of loaded documents
+        /// </summary>
         internal int PageCount { get; private set; }
 
         /// <summary>
@@ -48,12 +58,74 @@ namespace Specialized_PDF_Editor
                 reader.SetCloseStream(false);
 
                 PageCount = pdf.GetNumberOfPages();
-            } 
+
+                #region Another variant of load all page (simple but longer)
+#if false
+                {
+                    List<PdfPage> pages = new List<PdfPage>();
+                    for (int i = 0; i < PageCount; i++)
+                        pages.Add(pdf.GetPage(i + 1));
+                }
+#endif
+                #endregion
+
+                // get every page for analyse
+                PdfPage[] pages = Enumerable
+                            .Range(0, PageCount)
+                            .Select(t => pdf.GetPage(t + 1))
+                            .ToArray();
+
+                // create array for save information about every page
+                Pages = new PageInfo[pages.Length];
+
+                for (int i = 0; i < pages.Length; i++)
+                {
+                    Pages[i].Id = i + 1;
+                    Pages[i].Rotation = pages[i].GetRotation();
+                    Pages[i].Size = pages[i].GetPageSize();
+
+                    var dictionary = pdf.GetPage(i + 1).GetPdfObject().GetAsRectangle(PdfName.UserUnit);
+                }
+
+
+            }
         }
 
         public void Dispose()
         {
             stream?.Dispose();
         }
+    }
+
+    internal struct PageInfo
+    {
+        /// <summary>
+        /// Number of page
+        /// </summary>
+        internal int Id { get; set; }
+
+        /// <summary>
+        /// Page orientation
+        /// </summary>
+        internal int Rotation { get; set; }
+
+        /// <summary>
+        /// Size of page
+        /// </summary>
+        internal Rectangle Size { get; set; }
+
+        public override string ToString()
+            => new StringBuilder($"Number of page: {Id};")
+            .Append($"\nPage orientation: {Rotation} degree;")
+            .Append($"\nPage size:")
+            .Append($"\n\t - X: {Size.GetX()}")
+            .Append($"\n\t - Y: {Size.GetY()}")
+            .Append($"\n\t - Width: {Size.GetWidth()}")
+            .Append($"\n\t - Height: {Size.GetHeight()}")
+            .Append($"\n\t - Top: {Size.GetTop()}")
+            .Append($"\n\t - Bottom: {Size.GetBottom()}")
+            .Append($"\n\t - Left: {Size.GetLeft()}")
+            .Append($"\n\t - Right: {Size.GetRight()}")
+            .ToString();
     }
 }
